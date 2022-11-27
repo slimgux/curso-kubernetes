c8a72415486b364a4a04296b936862707b19e537
Gustavo Garcia Gonzalez <ggarciag@vectoritcgroup.com>
2022-11-27 07:34:37 +0100

    feat: modificaciones escalado, falta meterlo en variables de ambiente

--- usuarios/src/main/java/com/suki/curso/usuarios/infrastructure/clients/CursoClient.java
+++ usuarios/src/main/java/com/suki/curso/usuarios/infrastructure/clients/CursoClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cursos", url = "http://cursos:8002/cursos")
+@FeignClient(name = "cursos", url = "http://ec2-3-92-60-144.compute-1.amazonaws.com:8002/cursos")
 public interface CursoClient {
     @DeleteMapping("/eliminar-usuario-cursos/{usuarioId}")
     void eliminarUsuarioDeCursos(@PathVariable Long usuarioId);
